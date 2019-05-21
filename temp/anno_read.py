b30db81851ada344d0a37987c409709345f646d7
yhirama <yhirama@complex.ist.hokudai.ac.jp>
2019-05-21 20:14:17 +0900

    first commit

--- temp/anno_read.py
+++ temp/anno_read.py
@@ -1,12 +1,6 @@
+import numpy
+
 from scipy import io
-
-train_list = [str(i)+'_'+str(j) for i in range(1, 21) for j in range(1, 4)]
-val_list = [str(i)+'_'+str(j) for i in range(21, 27) for j in range(1, 4)]
-test_list = [str(i)+'_'+str(j) for i in range(27, 41) for j in range(1, 4)]
-
-matdata = io.loadmat('../data/Labels_MERL_Shopping_dataset/4_3_label.mat', squeeze_me=True)
-
-print(matdata['tlabs'])
 
 """
 0    Reach To Shelf (reach to shelf)
@@ -19,10 +13,52 @@ print(matdata['tlabs'])
 
 4    Inspect Shelf (look at shelf while not touching and not reaching for the shelf)
 """
-assert len(matdata['tlabs']) == 5, 'error'
-for i in range(5):
-    data = matdata['tlabs'][i]
-    assert len(data) > 0, 'error'
-    for j in data:
-        print(j)
 
+def read_path(num):
+    path = []
+    matdata = io.loadmat('../data/Labels_MERL_Shopping_dataset/{}_label.mat'.
+                         format(num), squeeze_me=True)
+    assert len(matdata['tlabs']) == 5, 'error'
+
+    for i in range(5):
+        data = matdata['tlabs'][i]
+        if len(data) == 0:
+            continue
+        # assert len(data) > 0, 'error data {}'.format(matdata['tlabs'])
+        if type(data[0]) is numpy.uint16:
+            data = [data]
+            print(data)
+        for j in data:
+            for k in range(j[0], j[1]):
+                path.append('all/{}_crop/image_{}.jpg {}'.format(num,
+                                                                 str(k).zfill(5),
+                                                                 i+1))
+                print(str(k).zfill(5))
+
+    return path
+
+if __name__=='__main__':
+    train_list = [str(i)+'_'+str(j) for i in range(1, 21) for j in range(1, 4)]
+    val_list = [str(i)+'_'+str(j) for i in range(21, 27) for j in range(1, 4)]
+    test_list = [str(i)+'_'+str(j) for i in range(27, 41) for j in range(1, 4)]
+
+    path = []
+    for i in train_list:
+        path.extend(read_path(i))
+    path = '\n'.join(path)
+    with open('../data/merl_label/train.txt', 'w') as f:
+        f.writelines(path)
+ 
+    path = []
+    for i in val_list:
+        path.extend(read_path(i))
+    path = '\n'.join(path)
+    with open('../data/merl_label/val.txt', 'w') as f:
+        f.writelines(path)
+ 
+    path = []
+    for i in test_list:
+        path.extend(read_path(i))
+    path = '\n'.join(path)
+    with open('../data/merl_label/test.txt', 'w') as f:
+        f.writelines(path)

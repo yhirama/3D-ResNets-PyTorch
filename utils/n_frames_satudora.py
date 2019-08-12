70601b8c9c03749a581e216aed824bfee037a362
yhirama <yhirama@complex.ist.hokudai.ac.jp>
2019-08-13 03:57:14 +0900

    /

---
+++ utils/n_frames_satudora.py
@@ -0,0 +1,36 @@
+from __future__ import division, print_function
+
+import os
+import sys
+import pathlib
+import subprocess
+
+
+def class_process(root_path):
+    root_path = pathlib.Path(root_path)
+    for cam_dir in root_path.iterdir():
+        for time_dir in cam_dir.iterdir():
+            if not os.path.isdir(time_dir):
+                return
+            img_path = pathlib.Path(time_dir, "images")
+
+            for file_name in img_path.iterdir():
+                image_indices = []
+                if 'jpg' not in file_name:
+                    continue
+                image_indices.append(file_name)
+
+            if len(image_indices) == 0:
+                print('no image files', file_name)
+                n_frames = 0
+            else:
+                image_indices.sort(reverse=True)
+                n_frames = len(image_indices)
+                print(time_dir, n_frames)
+            with open(str(pathlib.Path(img_path, 'n_frames')), 'w') as dst_file:
+                dst_file.write(str(n_frames))
+
+
+if __name__=="__main__":
+  root_path = sys.argv[1]
+  class_process(root_path)

523d046eef48e4885d3ecd1795d770a3d2e73a29
yhirama <yhirama@complex.ist.hokudai.ac.jp>
2019-08-13 03:29:19 +0900

    fix: bug

--- mv_data.py
+++ mv_data.py
@@ -17,19 +17,23 @@ def read_anno(root_path):
                     label = int(f.read())
                 file_name = anno_file_path.stem
                 img = Image.open(str(pathlib.Path(root_path,
+                                                  cam_dir.stem,
+                                                  time_dir.stem,
                                                   "images",
                                                   file_name + ".jpg")))
                 if label == 0:
                     output_path = str(pathlib.Path(
                         "/mnt/hdd3tb/action_recog/shelf",
                         cam_dir.stem,
-                        time_dir.stem
+                        time_dir.stem,
+                        "images"
                     ))
                 elif label == 1:
                     output_path = str(pathlib.Path(
                         "/mnt/hdd3tb/action_recog/others",
                         cam_dir.stem,
-                        time_dir.stem
+                        time_dir.stem,
+                        "images"
                     ))
                 else:
                     print("error")
@@ -37,7 +41,6 @@ def read_anno(root_path):
                 os.makedirs(output_path, exist_ok=True)
                 print(output_path)
                 img.save(str(pathlib.Path(output_path,
-                                          "images",
                                           file_name + ".jpg")))
 
 
